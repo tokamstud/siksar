0508e66a264c576ad6424e901c7d8964fb624830
tokams <t.gliniecki@hotmail.com>
2016-09-20 01:31:20 +0200

    optimization

--- ass1/pt2/sdes.cpp
+++ ass1/pt2/sdes.cpp
@@ -3,25 +3,27 @@
 #include <vector>
 #include <map>
 #include <bitset>
-#include <stdint.h>   // for uint32_t
-#include <limits.h>   // for CHAR_BIT
 #include <assert.h>
 #include <ctime>
 #include <time.h>
 #include <thread>
 #include <future>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
+std::mutex mtx;
+std::condition_variable cv;
+bool ready = false;
+
+
 int IP(int input) {
   int output = 0;
-
   vector<int> IP_table = {2,6,3,1,4,8,5,7};
-
   for (auto &p:IP_table) {
     output = (output<<1) | ((input >> ( 8-p ) ) & 1);
   }
-
   return output;
 }
 
@@ -37,11 +39,9 @@ int IPi(int input) {
 int P10(int input) {
   int output;
   vector<int> P10_table = {3,5,2,7,4,10,1,9,8,6};
-
   for (auto &p:P10_table) {
     output = (output<<1) | ((input >> ( 10-p ) ) & 1);
   }
-
   return output;
 }
 
@@ -51,7 +51,6 @@ int P8(int input) {
   for (auto &p:P8_table) {
     output = (output<<1) | ((input >> (10-p)) & 1);
   }
-
   return output;
 }
 
@@ -61,7 +60,6 @@ int P4(int input) {
   for (auto &p:P4_table) {
     output = (output<<1) | ((input >> (4-p)) & 1);
   }
-
   return output;
 }
 
@@ -82,10 +80,8 @@ int sBox(int input, short int sbox) {
   int output = 0;
   int row;
   int col;
-
   row = ((input >> 3) & 1);
   row = (row<<1) | ((input >> 0) & 1);
-
   col = ((input >> 2) & 1);
   col = (col<<1) | ((input >> 1) & 1);
 
@@ -114,45 +110,30 @@ std::vector<int> gen_keys(int key) {
   int combine;
   vector<int> keys;
   int output = P10(key);
-
   // split output into tow parts left and right
-  int left  = 0b1111100000 & output;
-  left >>=5;
-
+  int left  = 0b1111100000 & output;left >>=5;
   int right = 0b0000011111 & output;
 
   // leftshift by 1
-  left  = leftShift(left);
-  left &= 0b0000011111;
-  right = leftShift(right);
-  right &= 0b0000011111;
+  left  = leftShift(left)&0b0000011111;
+  right = leftShift(right)&0b0000011111;
 
-  combine = left;
-  combine <<=5;
-  combine &= 0b1111100000;
+  combine = (left<<5)&0b1111100000;
   combine |= right;
 
-  int key_1 = P8(combine);
-  key_1 &= 0b0011111111;
-
+  int key_1 = P8(combine)&0b0011111111;
   keys.push_back(key_1);
 
   // leftshift again by 2
   right = leftShift(right);
-  right = leftShift(right);
   left  = leftShift(left);
-  left  = leftShift(left);
+  right = leftShift(right)&0b0000011111;
+  left  = leftShift(left)&0b0000011111;
 
-  left  &= 0b0000011111;
-  right &= 0b0000011111;
-
-  combine = left;
-  combine <<= 5;
-  combine &= 0b1111100000;
+  combine = (left<<5)&0b1111100000;
   combine |= right;
 
-  int key_2 = P8(combine);
-  key_2 &= 0b0011111111;
+  int key_2 = P8(combine)&0b0011111111;
   keys.push_back(key_2);
 
   return keys;
@@ -168,17 +149,14 @@ int fBox(int input, int key) {
   // XOR key and 8 bits of EP
   output ^= key;
 
-  left = 0b11110000 & output;
-  left >>= 4;
+  left = (0b11110000 & output)>>4;
   right = 0b00001111 & output;
 
-  left = sBox(left,0);
-  right = sBox(right,1);
-  left &= 0b00000011;
-  right &= 0b00000011;
+  left = sBox(left,0)&0b00000011;
+  right = sBox(right,1)&0b00000011;
 
-  combine = left;
-  combine = (combine<<2) | right;
+  combine = (left<<2);
+  combine |= right;
 
   output = P4(combine);
 
@@ -191,8 +169,7 @@ int encrypt(int input, vector<int> keys) {
   output = IP(input);
 
   // split into 4bits
-  int left = 0b11110000 & output;
-  left >>= 4;
+  int left = (0b11110000 & output)>>4;
   int right = 0b00001111 & output;
 
   left ^= fBox(right,keys.at(0));
@@ -204,8 +181,8 @@ int encrypt(int input, vector<int> keys) {
 
   left ^= fBox(right,keys.at(1));
 
-  output = left;
-  output = (output<<4) | right;
+  output = left<<4;
+  output |= right;
 
   output = IPi(output);
 
@@ -218,8 +195,7 @@ int decrypt(int input, vector<int> keys) {
   output = IP(input);
 
   // split into 4bits
-  int left = 0b11110000 & output;
-  left >>= 4;
+  int left = (0b11110000 & output)>>4;
   int right = 0b00001111 & output;
 
   left ^= fBox(right,keys.at(1));
@@ -231,11 +207,10 @@ int decrypt(int input, vector<int> keys) {
 
   left ^= fBox(right,keys.at(0));
 
-  output = left;
-  output = (output<<4) | right;
+  output = left<<4;
+  output |= right;
 
   output = IPi(output);
-
 
   return output;
 }
@@ -293,9 +268,6 @@ char * read_to_mem(string file) {
     cxt.read (buffer,length);
 
     cxt.close();
-
-    // print content:
-    //std::cout.write (buffer,length);
   }
 
   return buffer;
@@ -339,9 +311,7 @@ int brutx_SDES(char* buffer) {
   return c_key;
 }
 
-vector<int> brutx_T2SDES(char* buffer) {
-
-  //char * buffer = read_to_mem(cxt);
+vector<int> brutx_T2SDES(char* buffer,int from = 0,int to = 1024) {
 
   int buff[60];
   for (size_t j = 0; j < 60; j++) {
@@ -357,7 +327,7 @@ vector<int> brutx_T2SDES(char* buffer) {
 
   vector<int> c_keys; // return key
 
-  for (size_t i = 0; i < 1024; i++) {
+  for (size_t i = from; i < to; i++) {
 
     for (size_t k = 0; k < 1024; k++) {
 
@@ -371,7 +341,6 @@ vector<int> brutx_T2SDES(char* buffer) {
             goto stop;
           }
         } else {
-          //cout << dec.size() << endl;
           dec.clear();
           break;
         }
@@ -387,16 +356,38 @@ vector<int> brutx_T2SDES(char* buffer) {
   return c_keys;
 }
 
-/*
-vector<int> T_brutx_T2SDES(char* cxt) {
-  vector<int> keys;
-
-  auto ret = async(brutx_T2SDES,cxt);
-  keys = ret.get();
-
-  return keys;
+void print_plain(char* cxt, int key, int key2=0,bool triple=false) {
+  int buff[60];
+  for (size_t j = 0; j < 60; j++) {
+    buff[j] = 0;
+    for (size_t i = 0; i < 8; i++) {
+      buff[j] = (buff[j]<<1) | ((int)cxt[(j*8)+i]-48);
+    }
+  }
+  if (triple) {
+    for (auto&block:buff) {
+      cout << (char)tripleSDESdec(block,key,key2);
+    }
+    cout << endl;
+  } else {
+    for (auto&block:buff) {
+      cout << (char)decrypt(block,gen_keys(key));
+    }
+    cout << endl;
+  }
 }
-*/
+
+void T_brutx_T2SDES(char* cxt, vector<int>& T_ks,int f, int t, string msg) {
+  vector<int> tmp = brutx_T2SDES(cxt,f,t);
+  if (tmp[0] > 0) {
+    T_ks = tmp;
+  }
+  cout << "Thread " << msg << " completed" << endl;
+  ready = true;
+}
+
+
+
 int main() {
   const string cxt1f("cxt1.txt");
   const string cxt2f("cxt2.txt");
@@ -405,25 +396,53 @@ int main() {
   char* cxt1 = read_to_mem(cxt1f);
   char* cxt2 = read_to_mem(cxt2f);
 
+  // Simplified DES - SDES
   clock_t beginSDES = clock();
   int ks = brutx_SDES(cxt1);
   if (ks<0) {
     cout << "Key could not be found" << endl;
   }
-  cout << ks << endl;
+  cout << "K1:"<<ks << endl;
+  print_plain(cxt1,ks);
   clock_t endSDES = clock();
-  cout << "Time   SDES: " << float(endSDES-beginSDES)/CLOCKS_PER_SEC << endl;
+  cout << "Time   SDES: " << float(endSDES-beginSDES)/CLOCKS_PER_SEC << " sek" << endl;
   cout << endl;
+
+  // Triple SDES T2SDES
   clock_t beginT2SDES = clock();
   vector<int> kt = brutx_T2SDES(cxt2);
   if (kt[0]<0) {
     cout << "Key could not be found" << endl;
   }
-  cout << kt[0] << endl;
-  cout << kt[1] << endl;
+  cout << "K1:"<<kt[0] << endl;
+  cout << "K2:"<<kt[1] << endl;
+  print_plain(cxt2,kt[0],kt[1],true);
   clock_t endT2SDES = clock();
-  cout << "Time T2SDES: " << float(endT2SDES-beginT2SDES)/CLOCKS_PER_SEC << endl;
-  cout << "TOTAL  TIME: " << float(endT2SDES-beginLoad1)/CLOCKS_PER_SEC << endl;
+  cout << "Time T2SDES: " << float(endT2SDES-beginT2SDES)/CLOCKS_PER_SEC << " sek" <<endl;
+  cout << endl<<"TOTAL  TIME: " << float(endT2SDES-beginLoad1)/CLOCKS_PER_SEC << " sek" <<endl;
+
+  /*
+  clock_t timeThread = clock();
+  vector<int> T_keys;
+  std::thread t1 (T_brutx_T2SDES,cxt2,std::ref(T_keys),0,256,"t1");
+  std::thread t2 (T_brutx_T2SDES,cxt2,std::ref(T_keys),256,512,"t2");
+  std::thread t3 (T_brutx_T2SDES,cxt2,std::ref(T_keys),512,768,"t3");
+  std::thread t4 (T_brutx_T2SDES,cxt2,std::ref(T_keys),1002,1024,"t4");
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+
+  std::unique_lock<std::mutex> lck(mtx);
+  while (!ready) cv.wait(lck);
+  cout << endl << endl;
+  cout << T_keys[0] << endl;
+  cout << T_keys[1] << endl;
+  clock_t stopThread = clock();
+
+  cout << "Time THREADS TIME: " << float(stopThread-timeThread)/CLOCKS_PER_SEC << endl;
+  */
+
 
   //cout << bitset<8>(tripleSDESenc(0b00000000,0b0000000000,0b0000000000)) << endl;
   //cout << bitset<8>(tripleSDESenc(0b11010111,0b1000101110,0b0110101110)) << endl;
