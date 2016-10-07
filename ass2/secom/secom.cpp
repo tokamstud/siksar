11cee6e2b6a8f05cfbfb66ed6e1ad74d6c2198cf
tokams <t.gliniecki@hotmail.com>
2016-10-07 11:19:05 +0200

    start

---
+++ ass2/secom/secom.cpp
@@ -0,0 +1,22 @@
+
+class client_secure32 {
+  protected private int pr_k;
+  public int pu_k;
+
+private:
+  int get_pr_k() {
+    // TODO:
+    return 0;
+  }
+
+  int generate_pr_k(int p) {
+    // TODO:
+    return 0;
+  }
+
+};
+
+int main() {
+
+  return 0;
+}
